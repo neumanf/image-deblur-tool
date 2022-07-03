252ff33a71a81c1e25940923069fd312773cda79
neumanf <fabricionewman@protonmail.com>
2022-07-03 16:48:51 -0300

    feat(api): add deployment configuration files

--- packages/api/main.py
+++ packages/api/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, File
 from fastapi.responses import StreamingResponse
 from deblur import ImageDeblur
@@ -11,3 +12,7 @@ async def deblur_image(image: bytes = File(...)):
     res = imageDeblurer.deblur(image)
 
     return StreamingResponse(res, media_type="image/png")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv(
+        "PORT", default=5000), log_level="info")
