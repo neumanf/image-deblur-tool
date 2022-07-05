ac19a008a8c8d61bae37aed176e338b60ce9c38a
neumanf <fabricionewman@protonmail.com>
2022-07-05 19:49:56 -0300

    feat(webapp): send request using filter parameters

--- packages/webapp/src/App.jsx
+++ packages/webapp/src/App.jsx
@@ -21,26 +21,39 @@ const navbarLinks = [
     },
 ];
 
+const isValidInput = (value) => {
+    if (isNaN(value) || value < 0 || value > 9999999) {
+        return false;
+    }
+    return true;
+};
+
 function App() {
+    const [isLoading, setLoading] = useState(false);
     const [imageSource, setImageSource] = useState(null);
     const [debluriedImage, setDebluriedImage] = useState(null);
     const [radius, setRadius] = useState(0);
     const [snr, setSNR] = useState(0);
 
     const deblurImage = async () => {
+        setLoading(true);
         const formData = new FormData();
         let image = document.querySelector("input[type=file]").files[0];
         formData.append("image", image);
 
-        const res = await fetch("http://127.0.0.1:8000/api/deblur", {
-            method: "POST",
-            headers: {
-                Accept: "*/*",
-            },
-            body: formData,
-        });
+        const res = await fetch(
+            `http://127.0.0.1:8000/api/deblur?r=${radius}&snr=${snr}`,
+            {
+                method: "POST",
+                headers: {
+                    Accept: "*/*",
+                },
+                body: formData,
+            }
+        );
         const blob = await res.blob();
         setDebluriedImage(URL.createObjectURL(blob));
+        setLoading(false);
     };
 
     return (
@@ -70,22 +83,42 @@ function App() {
                         <InputWrapper id="radius-input" label="R">
                             <NumberInput
                                 value={radius}
+                                onChange={(r) => setRadius(r)}
                                 type="number"
                                 id="radius-input"
                                 placeholder="Radius"
+                                error={
+                                    !isValidInput(radius) &&
+                                    "R must be a positive number"
+                                }
                                 required
                             />
                         </InputWrapper>
                         <InputWrapper id="snr-input" label="SNR">
                             <NumberInput
                                 value={snr}
+                                onChange={(snr) => setSNR(snr)}
                                 type="number"
                                 id="snr-input"
                                 placeholder="Signal-to-noise ratio"
+                                error={
+                                    !isValidInput(snr) &&
+                                    "SNR must be a positive number"
+                                }
                                 required
                             />
                         </InputWrapper>
-                        <Button onClick={deblurImage}>Deblur</Button>
+                        <Button
+                            onClick={deblurImage}
+                            disabled={
+                                imageSource === null ||
+                                !isValidInput(radius) ||
+                                !isValidInput(snr)
+                            }
+                            loading={isLoading}
+                        >
+                            Deblur
+                        </Button>
                     </Stack>
                     <Stack>
                         <Title order={4}>Result</Title>
